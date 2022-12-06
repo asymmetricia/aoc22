59f930b8d6bbc86de2734e65abae35fd0077505a
Tricia Bogen <tricia-github@cernu.us>
2022-12-05 22:35:42 -0800

    day6 + anim

--- day06/a/main.go
+++ day06/a/main.go
@@ -11,6 +11,18 @@ import (
 
 var log = logrus.StandardLogger()
 
+func isMarker(s string) bool {
+	if len(s) != 4 {
+		panic(s)
+	}
+	if s[0] == s[1] || s[0] == s[2] || s[0] == s[3] ||
+		s[1] == s[2] || s[1] == s[3] ||
+		s[2] == s[3] {
+		return false
+	}
+	return true
+}
+
 func solution(name string, input []byte) int {
 	// trim trailing space only
 	input = bytes.Replace(input, []byte("\r"), []byte(""), -1)
@@ -18,10 +30,13 @@ func solution(name string, input []byte) int {
 	lines := strings.Split(strings.TrimRightFunc(string(input), unicode.IsSpace), "\n")
 	log.Printf("read %d %s lines", len(lines), name)
 
-	//for _, line := range lines {
-	//	//fields := strings.Fields(line)
-	//}
-
+	for i := 0; i < len(input); i++ {
+		if !isMarker(string(input[i : i+4])) {
+			continue
+		}
+		log.Printf("%d, %s", i+4, input[i:i+4])
+		break
+	}
 	return -1
 }
 
--- day06/b/main.go
+++ day06/b/main.go
@@ -2,14 +2,103 @@ package main
 
 import (
 	"bytes"
+	"fmt"
+	"image/gif"
 	"os"
 	"strings"
 	"unicode"
 
 	"github.com/sirupsen/logrus"
+
+	"github.com/asymmetricia/aoc22/aoc"
+	"github.com/asymmetricia/aoc22/canvas"
 )
 
 var log = logrus.StandardLogger()
+
+func frame(s []rune, i int, n int, match bool) *canvas.Canvas {
+	c := aoc.TolVibrantRed
+	if match {
+		c = aoc.TolVibrantCyan
+	}
+
+	ret := &canvas.Canvas{}
+	canvas.TextBox{
+		Top:         0,
+		Left:        0,
+		Title:       []rune("Scanning for Start of Message"),
+		Footer:      []rune(fmt.Sprintf("characters %d..%d", i, i+n)),
+		TitleColor:  aoc.TolVibrantMagenta,
+		FrameColor:  aoc.TolVibrantTeal,
+		FooterColor: aoc.TolVibrantMagenta,
+		Width:       128,
+		Height:      64,
+	}.On(ret)
+outer:
+	for y := 0; y < 64; y++ {
+		for x := 0; x < 64; x++ {
+			idx := y*64 + x
+			posX := x*2 + 2
+			posY := y + 1
+			if idx >= len(s) {
+				break outer
+			}
+			if idx == i {
+				ret.Set(posX-1, posY, canvas.Cell{Value: '[', Color: c})
+			}
+			if idx == i+n {
+				ret.Set(posX-1, posY, canvas.Cell{Value: ']', Color: c})
+			}
+			if idx >= i && idx <= i+n {
+				ret.Set(posX, posY, canvas.Cell{Value: s[idx], Color: c})
+			} else {
+				ret.Set(posX, posY, canvas.Cell{Value: s[idx], Color: aoc.TolVibrantGrey})
+			}
+		}
+	}
+
+	if match {
+		canvas.TextBox{
+			Top:        3,
+			Center:     true,
+			Title:      []rune("Start Of Message Found!"),
+			Body:       s[i : i+n],
+			BodyBlock:  true,
+			BodyColor:  aoc.TolVibrantCyan,
+			TitleColor: aoc.TolVibrantTeal,
+			FrameColor: aoc.TolVibrantMagenta,
+		}.On(ret)
+	}
+
+	canvas.TextBox{
+		Top:        7 + aoc.LineHeight,
+		Center:     true,
+		Title:      []rune("Message Starts After"),
+		Body:       []rune(fmt.Sprintf("%04d", i+n)),
+		BodyBlock:  true,
+		BodyColor:  aoc.TolVibrantCyan,
+		TitleColor: aoc.TolVibrantTeal,
+		FrameColor: aoc.TolVibrantMagenta,
+		BodyFont:   aoc.SevenSeg,
+		TitleFont:  aoc.Pixl,
+	}.On(ret)
+
+	return ret
+}
+
+func isMarker(s string) bool {
+	if len(s) != 14 {
+		panic(s)
+	}
+	seen := map[byte]bool{}
+	for _, c := range []byte(s) {
+		if seen[c] {
+			return false
+		}
+		seen[c] = true
+	}
+	return true
+}
 
 func solution(name string, input []byte) int {
 	// trim trailing space only
@@ -18,9 +107,39 @@ func solution(name string, input []byte) int {
 	lines := strings.Split(strings.TrimRightFunc(string(input), unicode.IsSpace), "\n")
 	log.Printf("read %d %s lines", len(lines), name)
 
-	//for _, line := range lines {
-	//	//fields := strings.Fields(line)
-	//}
+	var frames []*canvas.Canvas
+	for i := 0; i < len(input); i++ {
+		match := isMarker(string(input[i : i+14]))
+		if match || i < 10 ||
+			i < 100 && i%2 == 0 ||
+			i < 200 && i%3 == 0 ||
+			i < 500 && i%5 == 0 ||
+			i%10 == 0 {
+			frame := frame([]rune(string(input)), i, 14, match)
+			frames = append(frames, frame)
+		}
+		if !match {
+			continue
+		}
+
+		log.Printf("%d, %s", i+14, input[i:i+14])
+		break
+	}
+
+	anim := &gif.GIF{}
+	for _, frame := range frames {
+		anim.Image = append(anim.Image, frame.Render())
+		anim.Delay = append(anim.Delay, 3)
+		anim.Disposal = append(anim.Disposal, gif.DisposalNone)
+	}
+	anim.Delay[len(anim.Delay)-1] = 1000
+
+	// Stick an extra frame on the end b/c of annoying transcoders
+	anim.Image = append(anim.Image, frames[len(frames)-1].Render())
+	anim.Delay = append(anim.Delay, 3)
+	anim.Disposal = append(anim.Disposal, gif.DisposalNone)
+
+	aoc.SaveGIF(anim, "day06b-"+name+".gif", log)
 
 	return -1
 }
