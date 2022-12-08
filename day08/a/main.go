85ebbfe77d1f4ed0c6d6eb681b4ded5215f5a45b
Tricia Bogen <tricia-github@cernu.us>
2022-12-07 22:48:06 -0800

    day 8 solved, iffy viz

--- day08/a/main.go
+++ day08/a/main.go
@@ -18,11 +18,67 @@ func solution(name string, input []byte) int {
 	lines := strings.Split(strings.TrimRightFunc(string(input), unicode.IsSpace), "\n")
 	log.Printf("read %d %s lines", len(lines), name)
 
-	//for _, line := range lines {
-	//	//fields := strings.Fields(line)
-	//}
+	trees := [][]int{}
+	for y, row := range lines {
+		trees = append(trees, make([]int, len(row)))
+		for x, height := range row {
+			trees[y][x] = int(height - '0')
+		}
+	}
 
-	return -1
+	visible := map[int]map[int]bool{}
+
+	for y, row := range trees {
+		visible[y] = map[int]bool{}
+		visible[y][0] = true
+		last := row[0]
+		for x, height := range row {
+			if height > last {
+				last = height
+				visible[y][x] = true
+			}
+		}
+
+		visible[y][len(row)-1] = true
+		last = row[len(row)-1]
+		for x := len(row) - 1; x >= 0; x-- {
+			if row[x] > last {
+				last = row[x]
+				visible[y][x] = true
+			}
+		}
+	}
+
+	for x := 0; x < len(trees[0]); x++ {
+		visible[0][x] = true
+		last := trees[0][x]
+		for y, row := range trees {
+			if row[x] > last {
+				last = row[x]
+				visible[y][x] = true
+			}
+		}
+
+		visible[len(trees)-1][x] = true
+		last = trees[len(trees)-1][x]
+		for y := len(trees) - 1; y >= 0; y-- {
+			if trees[y][x] > last {
+				last = trees[y][x]
+				visible[y][x] = true
+			}
+		}
+	}
+
+	count := 0
+	for _, row := range visible {
+		for _, tree := range row {
+			if tree {
+				count++
+			}
+		}
+	}
+
+	return count
 }
 
 func main() {
--- day08/b/main.go
+++ day08/b/main.go
@@ -2,14 +2,93 @@ package main
 
 import (
 	"bytes"
+	"image/color"
+	"math"
 	"os"
+	"strconv"
 	"strings"
 	"unicode"
 
 	"github.com/sirupsen/logrus"
+
+	"github.com/asymmetricia/aoc22/aoc"
+	"github.com/asymmetricia/aoc22/canvas"
+	"github.com/asymmetricia/aoc22/coord"
 )
 
 var log = logrus.StandardLogger()
+
+func visible(trees [][]int, x, y int) map[coord.Direction][]coord.Coord {
+	ret := map[coord.Direction][]coord.Coord{}
+
+	height := trees[y][x]
+
+	for north := y - 1; north >= 0; north-- {
+		ret[coord.North] = append(ret[coord.North], coord.C(x, north))
+		if trees[north][x] >= height {
+			break
+		}
+	}
+
+	for south := y + 1; south < len(trees); south++ {
+		ret[coord.South] = append(ret[coord.South], coord.C(x, south))
+		if trees[south][x] >= height {
+			break
+		}
+	}
+
+	for west := x - 1; west >= 0; west-- {
+		ret[coord.West] = append(ret[coord.West], coord.C(west, y))
+		if trees[y][west] >= height {
+			break
+		}
+	}
+
+	for east := x + 1; east < len(trees[0]); east++ {
+		ret[coord.East] = append(ret[coord.East], coord.C(east, y))
+		if trees[y][east] >= height {
+			break
+		}
+	}
+
+	return ret
+}
+
+func frame(vis map[int]map[int]int, trees [][]int, x, y int, v map[coord.Direction][]coord.Coord, best coord.Coord) *canvas.Canvas {
+	ret := &canvas.Canvas{}
+
+	for yy, row := range trees {
+		for xx, height := range row {
+			var c color.Color = color.White
+			if yy < y || yy == y && xx < x {
+				c = aoc.TolVibrantGrey
+			}
+			if xx == x && yy == y {
+				c = aoc.TolVibrantOrange
+			}
+			ret.PrintAt(xx*2, yy, strconv.Itoa(height), c)
+		}
+	}
+
+	for _, visibleTrees := range v {
+		for _, tree := range visibleTrees {
+			xx := tree.X
+			yy := tree.Y
+			ret.PrintAt(xx*2, yy, strconv.Itoa(trees[yy][xx]), aoc.TolVibrantCyan)
+		}
+	}
+
+	if best.Y > 0 && best.X > 0 {
+		canvas.TextBox{
+			Top:       best.Y - 1,
+			Left:      best.X*2 - 1,
+			Body:      []rune("+"),
+			BodyColor: aoc.TolVibrantRed,
+		}.On(ret)
+	}
+
+	return ret
+}
 
 func solution(name string, input []byte) int {
 	// trim trailing space only
@@ -18,11 +97,55 @@ func solution(name string, input []byte) int {
 	lines := strings.Split(strings.TrimRightFunc(string(input), unicode.IsSpace), "\n")
 	log.Printf("read %d %s lines", len(lines), name)
 
-	//for _, line := range lines {
-	//	//fields := strings.Fields(line)
-	//}
+	trees := [][]int{}
+	for y, row := range lines {
+		trees = append(trees, make([]int, len(row)))
+		for x, height := range row {
+			trees[y][x] = int(height - '0')
+		}
+	}
 
-	return -1
+	framesMap := map[int]map[int]*canvas.Canvas{}
+	visibility := map[int]map[int]int{}
+
+	var best coord.Coord
+	for y, row := range trees {
+		visibility[y] = map[int]int{}
+		framesRow := map[int]*canvas.Canvas{}
+		for x := range row {
+			visibleTrees := visible(trees, x, y)
+			score := len(visibleTrees[coord.North]) *
+				len(visibleTrees[coord.East]) *
+				len(visibleTrees[coord.South]) *
+				len(visibleTrees[coord.West])
+			visibility[y][x] = score
+			if score > visibility[best.Y][best.X] {
+				log.Print(score)
+				best = coord.C(x, y)
+			}
+			if x > 0 && x < len(row)-1 && y > 0 && y < len(trees)-1 {
+				framesRow[x] = frame(visibility, trees, x, y, visibleTrees, best)
+			}
+		}
+		framesMap[y] = framesRow
+		if y%10 == 0 {
+			log.Printf("y=%d", y)
+		}
+	}
+
+	log.Print("computation finished")
+
+	var frames []*canvas.Canvas
+	for y, row := range trees {
+		for x := range row {
+			if framesMap[y][x] != nil {
+				frames = append(frames, framesMap[y][x])
+			}
+		}
+	}
+	canvas.RenderGif(frames, map[int]float32{10: 50, 20: 25, 40: 12, 150: 3, 300: 1.0 / 2, math.MaxInt: 1.0 / 4}, "day08b-"+name+".gif", log)
+
+	return visibility[best.Y][best.X]
 }
 
 func main() {
