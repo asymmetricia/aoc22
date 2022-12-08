afd101361959cb774b6e2890bc41d373ee6026f2
Tricia Bogen <tricia-github@cernu.us>
2022-12-07 22:47:47 -0800

    parallelize rendering

--- canvas/gif.go
+++ canvas/gif.go
@@ -1,8 +1,10 @@
 package canvas
 
 import (
+	"image"
 	"image/gif"
 	"math"
+	"sync"
 
 	"github.com/sirupsen/logrus"
 
@@ -29,13 +31,18 @@ func timing(n int, csPerFrame map[int]float32) float32 {
 func RenderGif(canvases []*Canvas, csPerFrame map[int]float32, filename string, log logrus.FieldLogger) {
 	width, height := Bounds(canvases)
 	anim := &gif.GIF{}
-	var i int
-	var canvas *Canvas
-	var frameTiming float32
-	for i, canvas = range canvases {
-		if csPerFrame == nil {
-			frameTiming = 3
-		} else {
+
+	type Frame struct {
+		img    *image.Paletted
+		timing int
+	}
+
+	frames := &sync.Map{}
+	wg := &sync.WaitGroup{}
+
+	for i, canvas := range canvases {
+		var frameTiming float32 = 3
+		if csPerFrame != nil {
 			frameTiming = timing(i, csPerFrame)
 			if frameTiming < 1 {
 				if i%(int(1.0/frameTiming)) != 0 && i != len(canvases)-1 {
@@ -45,14 +52,34 @@ func RenderGif(canvases []*Canvas, csPerFrame map[int]float32, filename string, 
 			}
 		}
 
-		anim.Image = append(anim.Image, canvas.RenderRect(width, height))
-		anim.Delay = append(anim.Delay, int(frameTiming))
-		anim.Disposal = append(anim.Disposal, gif.DisposalNone)
-
+		wg.Add(1)
+		go func(i int, canvas *Canvas, frameTiming float32) {
+			defer wg.Done()
+			if len(canvases) < 10 || i%(len(canvases)/10) == 0 {
+				log.Printf("rendering frame %d", i)
+			}
+			frames.Store(i, Frame{canvas.RenderRect(width, height), int(frameTiming)})
+		}(i, canvas, frameTiming)
 	}
 
-	anim.Image = append(anim.Image, canvas.RenderRect(width, height))
-	anim.Delay = append(anim.Delay, int(frameTiming))
+	wg.Wait()
+
+	for i := range canvases {
+		frame, ok := frames.Load(i)
+		if !ok {
+			continue
+		}
+		anim.Image = append(anim.Image, frame.(Frame).img)
+		anim.Delay = append(anim.Delay, frame.(Frame).timing)
+		anim.Disposal = append(anim.Disposal, gif.DisposalNone)
+	}
+
+	lastImage := anim.Image[len(anim.Image)-1]
+	img := image.NewPaletted(lastImage.Rect, lastImage.Palette)
+	copy(img.Pix, lastImage.Pix)
+
+	anim.Image = append(anim.Image, lastImage)
+	anim.Delay = append(anim.Delay, anim.Delay[len(anim.Delay)-1])
 	anim.Disposal = append(anim.Disposal, gif.DisposalNone)
 
 	anim.Delay[len(anim.Delay)-2] = 500
