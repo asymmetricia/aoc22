56e9dfeb536c11bd7a82f98d85eb7f7169295604
Tricia Bogen <tricia-github@cernu.us>
2022-12-09 21:29:36 -0800

    day10 + viz

--- day10/b/main.go
+++ day10/b/main.go
@@ -3,13 +3,88 @@ package main
 import (
 	"bytes"
 	"os"
+	"strconv"
 	"strings"
 	"unicode"
 
 	"github.com/sirupsen/logrus"
+
+	"github.com/asymmetricia/aoc22/aoc"
+	"github.com/asymmetricia/aoc22/canvas"
 )
 
 var log = logrus.StandardLogger()
+
+type CPU struct {
+	X           int
+	Cycle       int
+	SS          int
+	Signal      int
+	FrameBuffer [240]bool
+	Frames      []*canvas.Canvas
+}
+
+func (c *CPU) Noop() {
+	c.Cycle++
+	c.Draw()
+}
+
+func (c *CPU) Addx(i int) {
+	c.Cycle++
+	c.Draw()
+	c.Cycle++
+	c.Draw()
+	c.X += i
+}
+
+func (c *CPU) Draw() {
+	if (c.Cycle+20)%40 == 0 {
+		c.Signal += c.Cycle * c.X
+	}
+
+	cycle := c.Cycle - 1
+	if cycle%40 >= c.X-1 && cycle%40 <= c.X+1 {
+		c.FrameBuffer[cycle] = true
+	}
+	cnv := &canvas.Canvas{}
+	canvas.TextBox{
+		Title:      []rune("Elfosonic"),
+		FrameColor: aoc.TolVibrantBlue,
+		Width:      40,
+		Height:     6,
+	}.On(cnv)
+	for y := 0; y < 6; y++ {
+		for x := 0; x < 40; x++ {
+			if c.FrameBuffer[40*y+x] {
+				cnv.PrintAt(x+1, y+1, "#", aoc.TolVibrantTeal)
+			} else {
+				cnv.PrintAt(x+1, y+1, ".", aoc.TolVibrantGrey)
+			}
+		}
+	}
+	canvas.TextBox{
+		Left:  42,
+		Title: []rune("Cycle"),
+		Body:  []rune(strconv.Itoa(c.Cycle)),
+		Width: 6,
+	}.On(cnv)
+	canvas.TextBox{
+		Top:   2,
+		Left:  42,
+		Title: []rune("X"),
+		Body:  []rune(strconv.Itoa(c.X)),
+		Width: 6,
+	}.On(cnv)
+	canvas.TextBox{
+		Top:   4,
+		Left:  42,
+		Title: []rune("Signal"),
+		Body:  []rune(strconv.Itoa(c.Signal)),
+		Width: 6,
+	}.On(cnv)
+	cnv.PrintAt(42, 7, " ⓒElfTek", aoc.TolVibrantMagenta)
+	c.Frames = append(c.Frames, cnv)
+}
 
 func solution(name string, input []byte) int {
 	// trim trailing space only
@@ -18,11 +93,35 @@ func solution(name string, input []byte) int {
 	lines := strings.Split(strings.TrimRightFunc(string(input), unicode.IsSpace), "\n")
 	log.Printf("read %d %s lines", len(lines), name)
 
-	//for _, line := range lines {
-	//	//fields := strings.Fields(line)
-	//}
+	cpu := &CPU{X: 1}
+	for _, line := range lines {
+		parts := strings.Fields(line)
+		switch parts[0] {
+		case "noop":
+			cpu.Noop()
+		case "addx":
+			immed, err := strconv.Atoi(parts[1])
+			if err != nil {
+				panic(err)
+			}
+			cpu.Addx(immed)
+		}
+	}
 
-	return -1
+	for y := 0; y < 6; y++ {
+		for x := 0; x < 40; x++ {
+			if cpu.FrameBuffer[y*40+x] {
+				print("#")
+			} else {
+				print(".")
+			}
+		}
+		println()
+	}
+
+	canvas.RenderGif(cpu.Frames, nil, "day10-"+name+".gif", log)
+
+	return cpu.SS
 }
 
 func main() {
