86c8040cc99341257c0f03f71ed6a87819461663
Tricia Bogen <tricia-github@cernu.us>
2022-12-16 00:52:40 -0800

    part A & too slow part B

--- day16/a/main.go
+++ day16/a/main.go
@@ -2,16 +2,89 @@ package main
 
 import (
 	"bytes"
+	"fmt"
+	"math"
 	"os"
+	"sort"
 	"strings"
 	"unicode"
 
 	"github.com/sirupsen/logrus"
+	"golang.org/x/exp/maps"
 
 	"github.com/asymmetricia/aoc22/aoc"
 )
 
 var log = logrus.StandardLogger()
+
+type Valve struct {
+	Rate      int
+	Neighbors map[string]*Valve
+	Peers     []string
+	Open      bool
+}
+
+func (v *Valve) String() string {
+	return fmt.Sprintf("{rate=%d, neighbors=%v}", v.Rate, v.Peers)
+}
+
+// Explore considers moving to each possible unopened valve. It returns the best
+// discovered order of valve openings, and an integer describing the total rate
+// released in this process.
+func Explore(pos string, network map[string]*Valve, unopened map[string]bool, minutes int, openRate int) ([]string, int) {
+	var best int
+	var bestPath []string
+	var ret = 0
+
+	// we could not actually get here lol
+	if minutes < 0 {
+		return nil, math.MinInt
+	}
+
+	// open this valve
+	if minutes > 0 && network[pos].Rate > 0 {
+		minutes--
+		ret += openRate
+		openRate += network[pos].Rate
+	}
+
+	// nowhere left to go, let them run
+	if len(unopened) == 0 {
+		return nil, ret + openRate*minutes
+	}
+
+	uo := maps.Keys(unopened)
+	sort.Strings(uo)
+	for i, next := range uo {
+		// Compute a path to the next unopened valve
+		path := aoc.Dijkstra(pos, next, func(a string) []string {
+			return maps.Keys(network[a].Neighbors)
+		}, func(a, b string) int {
+			return 1
+		})
+		path = path[1:]
+
+		if len(path) > minutes {
+			continue
+		}
+
+		// we will open this valve
+		candidateUnopened := map[string]bool{}
+		maps.Copy(candidateUnopened, unopened)
+		delete(candidateUnopened, next)
+
+		candidate, value := Explore(next, network, candidateUnopened, minutes-len(path), openRate)
+		value += openRate * len(path)
+		if value > best {
+			best = value
+			bestPath = append([]string{next}, candidate...)
+		}
+		if pos == "AA" {
+			log.Printf("%d/%d == %d", i, len(uo), ret+value)
+		}
+	}
+	return bestPath, ret + best
+}
 
 func solution(name string, input []byte) int {
 	// trim trailing space only
@@ -20,11 +93,35 @@ func solution(name string, input []byte) int {
 	lines := strings.Split(strings.TrimRightFunc(string(input), unicode.IsSpace), "\n")
 	log.Printf("read %d %s lines", len(lines), name)
 
-	//for _, line := range lines {
-	//	//fields := strings.Fields(line)
-	//}
+	network := map[string]*Valve{}
 
-	return -1
+	for _, line := range lines {
+		fields := strings.Fields(line)
+		name := fields[1]
+		rate := aoc.MustAtoi(aoc.Before(aoc.After(fields[4], "="), ";"))
+		peers := fields[9:]
+		for i := range peers {
+			peers[i] = strings.Trim(peers[i], ",")
+		}
+		network[name] = &Valve{Rate: rate, Peers: peers, Neighbors: map[string]*Valve{}}
+		// 30min total
+	}
+
+	valves := map[string]bool{}
+	for id, valve := range network {
+		if valve.Rate > 0 {
+			valves[id] = true
+		}
+		for _, peer := range valve.Peers {
+			valve.Neighbors[peer] = network[peer]
+		}
+	}
+
+	log.Print(network)
+	path, value := Explore("AA", network, valves, 30, 0)
+	log.Print(path, value)
+
+	return value
 }
 
 func main() {
--- day16/b/main.go
+++ day16/b/main.go
@@ -2,16 +2,249 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"os"
+	"sort"
+	"strconv"
 	"strings"
 	"unicode"
 
 	"github.com/sirupsen/logrus"
+	"golang.org/x/exp/maps"
 
 	"github.com/asymmetricia/aoc22/aoc"
 )
 
 var log = logrus.StandardLogger()
+
+type Valve struct {
+	Rate      int
+	Neighbors map[string]*Valve
+	Peers     []string
+}
+
+func (v *Valve) String() string {
+	return fmt.Sprintf("{rate=%d, neighbors=%v}", v.Rate, v.Peers)
+}
+
+type State struct {
+	Network      map[string]*Valve `json:"-"`
+	Pos          string
+	Goal         string
+	ElephantPos  string
+	ElephantGoal string
+	Open         map[string]bool
+	Minutes      int
+}
+
+func (s *State) CK() string {
+	var ret strings.Builder
+	ret.WriteString(s.Pos)
+	ret.WriteRune('|')
+	ret.WriteString(s.Goal)
+	ret.WriteRune('|')
+	ret.WriteString(s.ElephantPos)
+	ret.WriteRune('|')
+	ret.WriteString(s.ElephantGoal)
+	ret.WriteRune('|')
+	ret.WriteString(strconv.Itoa(s.Minutes))
+	ret.WriteRune('|')
+	opens := maps.Keys(s.Open)
+	sort.Strings(opens)
+	ret.WriteString(strings.Join(opens, ","))
+	return ret.String()
+}
+
+func (s *State) Copy() *State {
+	return &State{
+		Network:      s.Network,
+		Pos:          s.Pos,
+		Goal:         s.Goal,
+		ElephantPos:  s.ElephantPos,
+		ElephantGoal: s.ElephantGoal,
+		Minutes:      s.Minutes,
+		Open:         maps.Clone(s.Open),
+	}
+}
+
+func (s *State) HumanAction(valves []string) []*State {
+	//ck := "ha_" + s.CK()
+
+	//checks++
+	//if cache, ok := cache[ck]; ok {
+	//	hits++
+	//	return cache
+	//}
+
+	if s.Pos == s.Goal && !s.Open[s.Pos] {
+		ret := s.Copy()
+		ret.Open[s.Pos] = true
+		return []*State{ret}
+	}
+
+	if s.Pos != s.Goal {
+		path := paths[[2]string{s.Pos, s.Goal}]
+		ret := s.Copy()
+		ret.Pos = path[0]
+		return []*State{ret}
+	}
+
+	var ret []*State
+	for _, goal := range valves {
+		if s.Open[goal] || s.ElephantGoal == goal {
+			continue
+		}
+
+		path := paths[[2]string{s.Pos, goal}]
+
+		//skip goals that are too far away
+		//if len(path)+2 > s.Minutes {
+		//	continue
+		//}
+
+		n := s.Copy()
+		n.Goal = goal
+		n.Pos = path[0]
+		ret = append(ret, n)
+	}
+
+	//cache[ck] = ret
+	return ret
+}
+func (s *State) ElephantAction(valves []string) []*State {
+	//ck := "ea_" + s.CK()
+
+	//checks++
+	//if cache, ok := cache[ck]; ok {
+	//	hits++
+	//	return cache
+	//}
+
+	if s.ElephantPos == s.ElephantGoal && !s.Open[s.ElephantPos] {
+		ret := s.Copy()
+		ret.Open[s.ElephantPos] = true
+		return []*State{ret}
+	}
+
+	if s.ElephantPos != s.ElephantGoal {
+		path := paths[[2]string{s.ElephantPos, s.ElephantGoal}]
+		ret := s.Copy()
+		ret.ElephantPos = path[0]
+		return []*State{ret}
+	}
+
+	var ret []*State
+	for _, goal := range valves {
+		if s.Open[goal] || s.Goal == goal {
+			continue
+		}
+
+		path := paths[[2]string{s.ElephantPos, goal}]
+
+		//skip goals that are too far away
+		// len(path) == time to get there
+		// 1 == time to open valve
+		// 1 == time for opening valve to be worthwhile
+		//if len(path)+2 > s.Minutes {
+		//	continue
+		//}
+
+		n := s.Copy()
+		n.ElephantGoal = goal
+		n.ElephantPos = path[0]
+		ret = append(ret, n)
+	}
+
+	//cache[ck] = ret
+	return ret
+}
+
+var cache = map[string][]*State{}
+var checks = 0
+var hits = 0
+
+func Act(s *State, depth int) []*State {
+	if s.Minutes == 0 {
+		return nil
+	}
+
+	ck := s.CK()
+	checks++
+	if cached, ok := cache[ck]; ok {
+		hits++
+		return cached
+	}
+
+	var candidates []*State
+	for _, state := range s.HumanAction(maps.Keys(s.Network)) {
+		ecs := state.ElephantAction(maps.Keys(s.Network))
+		if len(ecs) > 0 {
+			candidates = append(candidates, ecs...)
+		} else {
+			candidates = append(candidates, state)
+		}
+	}
+
+	var best int
+	var bestCandidate []*State
+	//type seenKey struct {
+	//	P, G, P2, G2 string
+	//}
+	//seen := map[seenKey]bool{}
+	for i, cs := range candidates {
+		cs.Minutes--
+
+		//sk := seenKey{cs.Pos, cs.Goal, cs.ElephantPos, cs.ElephantGoal}
+		//rk := seenKey{cs.ElephantPos, cs.ElephantGoal, cs.Pos, cs.Goal}
+		//if seen[sk] || seen[rk] {
+		//	continue
+		//}
+		//seen[sk] = true
+
+		if depth <= 1 {
+			log.Printf("chr %d%% (%d/%d) depth %d, %d/%d, best -> %d", hits*100/checks, hits, checks, depth, i, len(candidates), best)
+			//log.Printf("depth %d, %d/%d, best -> %d", depth, i, len(candidates), best)
+		}
+		result := Act(cs, depth+1)
+		if Value(result) > best {
+			best = Value(result)
+			bestCandidate = result
+		}
+	}
+
+	ret := append([]*State{s}, bestCandidate...)
+	cache[ck] = ret
+	return ret
+}
+
+func (s State) String() string {
+	var opens []string
+	rate := 0
+	for valve := range s.Open {
+		rate += s.Network[valve].Rate
+		if s.Network[valve].Rate > 0 {
+			opens = append(opens, valve)
+		}
+	}
+	sort.Strings(opens)
+	return fmt.Sprintf("@%d me=%s->%s, el=%s->%s, open %v, rate %d", s.Minutes, s.Pos, s.Goal, s.ElephantPos, s.ElephantGoal, opens, rate)
+}
+
+func Value(states []*State) int {
+	ret := 0
+	for i, state := range states {
+		for open := range state.Open {
+			if i == len(states)-1 {
+				ret += state.Network[open].Rate * state.Minutes
+			} else {
+				ret += state.Network[open].Rate
+			}
+		}
+	}
+	return ret
+}
+
+var paths = map[[2]string][]string{}
 
 func solution(name string, input []byte) int {
 	// trim trailing space only
@@ -20,11 +253,73 @@ func solution(name string, input []byte) int {
 	lines := strings.Split(strings.TrimRightFunc(string(input), unicode.IsSpace), "\n")
 	log.Printf("read %d %s lines", len(lines), name)
 
-	//for _, line := range lines {
-	//	//fields := strings.Fields(line)
-	//}
+	network := map[string]*Valve{}
 
-	return -1
+	for _, line := range lines {
+		fields := strings.Fields(line)
+		name := fields[1]
+		rate := aoc.MustAtoi(aoc.Before(aoc.After(fields[4], "="), ";"))
+		peers := fields[9:]
+		for i := range peers {
+			peers[i] = strings.Trim(peers[i], ",")
+		}
+		network[name] = &Valve{Rate: rate, Peers: peers, Neighbors: map[string]*Valve{}}
+		// 30min total
+	}
+
+	start := State{
+		Network:      network,
+		Pos:          "AA",
+		Goal:         "AA",
+		ElephantPos:  "AA",
+		ElephantGoal: "AA",
+		Open:         map[string]bool{},
+		Minutes:      26,
+	}
+
+	for id, valve := range network {
+		if valve.Rate == 0 {
+			start.Open[id] = true
+		}
+		for _, peer := range valve.Peers {
+			valve.Neighbors[peer] = network[peer]
+		}
+	}
+
+	paths = map[[2]string][]string{}
+	for a := range network {
+		for b := range network {
+			if a == b {
+				continue
+			}
+
+			path := aoc.Dijkstra(a, b, func(a string) []string {
+				return maps.Keys(network[a].Neighbors)
+			}, aoc.ConstantCost[string])
+			path = path[1:]
+			paths[[2]string{a, b}] = path
+		}
+	}
+
+	//cache = map[string][]*State{}
+	//checks = 0
+	//hits = 0
+	if name == "test" {
+		for i := 0; i < 10; i++ {
+			path := Act(&start, 1)
+			if Value(path) != 1707 {
+				panic("nope")
+			}
+		}
+	}
+	log.Print(network)
+	path := Act(&start, 1)
+	for _, s := range path {
+		log.Print(s)
+	}
+	log.Print(Value(path))
+
+	return Value(path)
 }
 
 func main() {
